e70b52c64242fbd067f235e1b74f54652c64fe97
Yerlan <ersag82@gmail.com>
2022-11-08 20:06:11 +0600

    second slider 08.11.22

--- src/components/DropMenu.js
+++ src/components/DropMenu.js
@@ -1,103 +1,145 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
 
 const DropMenu = ({focus, setFocus}) => {
 
 return (
-    <section className={focus?'dropmenu dropmenu-active': 'dropmenu dropmenu-closed'} >
+<section className={focus?'dropmenu dropmenu-active': 'dropmenu dropmenu-closed'} >
 <div className="dropContent">
+        <Link to='#'>
         <h2 className="dropTitle">
             sjgfkdjgdk
         </h2>
+        </Link>
         <p className="dropText">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         </p>
         <ul className="dropList">
-            <li className="dropItem">
+            <Link to ='#'>    
+               <li className="dropItem">
                 sfsf
-            </li>
-            <li className="dropItem">
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
                 sfsf
-            </li>
-            <li className="dropItem">
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
                 sfsf
-            </li>
-            <li className="dropItem">
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
                 sfsf
-            </li>
+               </li>
+            </Link>
         </ul>
     
 </div>
 <div className="dropContent">
+        <Link to='#'>
         <h2 className="dropTitle">
             sjgfkdjgdk
         </h2>
+        </Link>
         <p className="dropText">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         </p>
         <ul className="dropList">
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
         </ul>
     
 </div>
 <div className="dropContent">
+        <Link to='#'>
         <h2 className="dropTitle">
             sjgfkdjgdk
         </h2>
+        </Link>
         <p className="dropText">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         </p>
         <ul className="dropList">
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
         </ul>
     
 </div>
 <div className="dropContent">
+        <Link to='#'>
         <h2 className="dropTitle">
             sjgfkdjgdk
         </h2>
+        </Link>
         <p className="dropText">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         </p>
         <ul className="dropList">
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
-            <li className="dropItem">
-                <a href="#">sfsf</a>
-            </li>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
+            <Link to ='#'>    
+               <li className="dropItem">
+                sfsf
+               </li>
+            </Link>
         </ul>
     
 </div>
+
     </section>
 )
 }
--- src/components/SecondSlider.js
+++ src/components/SecondSlider.js
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import data2 from '../data2';
 import { Link } from 'react-router-dom';
+import {FiChevronRight} from 'react-icons/fi'
+
 
 
 function SecondSlider() {
   const [item, setItem] = useState(data2);
   const [index, setIndex] = useState(0);
+  useEffect(()=> {
+if (index>=item.length-2) {
+  setIndex(0);
+}
+  }, [index, item])
+  
     return ( 
     <>
     <div className="secondSlider-content">
-      {item.map((prod) => {
+      {item.map((prod, prodIndex) => {
+        const{id, img, textTitle, mainText} = prod;
             return (
-  <>
   
-  <div className="secondSliderContainer">
-    <div className="secondSlider-inner">
-    <img src={prod.img} alt="product image" className="secondSliderImg" />
-    <h2 className='secondSliderTitle'><Link to='#'>{prod.textTitle}</Link></h2>
-    <p className="secondSliderText">{prod.mainText}</p>
+    <div className="secondSliderContainer" key={id}>
+    <div className="secondSlider-inner" style={{transform:`translateX(-${index*360}px)`}}>
+    <img src={img} alt="product" className="secondSliderImg" />
+    <h2 className='secondSliderTitle'><Link to='#'>{textTitle}</Link></h2>
+    <p className="secondSliderText">{mainText}</p>
     </div>
+    
   </div>
- 
- 
-  </>
+  
 )
       })}
+      
+      <button className="secondSliderNext-btn" onClick={()=>{setIndex(index+1)}}><FiChevronRight/></button>
      </div>
      </>
      );
