c39adb3aefac79510234d7ccf08813cf4647d0f5
Yerlan <ersag82@gmail.com>
2022-11-17 21:46:11 +0600

    18.11.22 вопросы и текст

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Questions() {
     return (
-      <p>questions</p>
+      <>
+      <div className="questions-content">
+        <div className="questions-inner">
+          <h2 className="questions-header"><Link to='#'>Вопросы и ответы</Link></h2>
+          <p className='questions-title'><Link to='#' >Вопросы по применению продукции</Link></p>
+          <p className='questions-title'><Link to='#' >Коммерческие вопросы</Link></p>
+          <p className='questions-title'><Link to='#' >Общие вопросы</Link></p> 
+        </div>
+      </div>
+      </>
     );
   }
   export default Questions;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,9 @@ import {FaQuestion} from "react-icons/fa";
 import Form from "../components/Form";
 import News from "../components/News"
 import { useState } from "react";
+import {Link} from 'react-router-dom';
+import garanty from '../assets/hometextIcons/garanty.png'
+import rosizol from '../assets/hometextIcons/rosizol.png'
 
 function Home() {
   const [showForm, setShowForm] = useState(false);
@@ -31,16 +34,15 @@ function Home() {
         <Questions/>
       </div>
       <div className="homeText">
-        <h2>Ursa-leader and expert</h2>
-        <p>Компания URSA — Ваш партнeр в области строительных изоляционных материалов 
+        <h2 className='homeText-title'>Ursa-leader and expert</h2>
+        <p className='homeText-text'>Компания URSA — Ваш партнeр в области строительных изоляционных материалов 
           и систем утепления для различных конструкций. URSA — одна из ведущих компаний 
           на строительном рынке Европы и один из самых известных брендов строительных 
           материалов. URSA предлагает широкий ассортимент тепло-и звукоизоляционных 
           материалов для строительства новых и реконструкции существующих зданий и сооружений.</p>
       <div className="homeIcon_container">
-        <img src="https://www.ursa.ru/why_ursa/preimushchestva-materialov/garantiya-50-let/"
-         alt="logo" />
-         <img src="http://rosizol.org/" alt="member of Rosisol association" />
+         <Link to = '#'><img src= {garanty} alt = "garanty icon" className='garantyIcon'/></Link>
+         <Link to = '#'><img src= {rosizol} alt = "rosizol icon" className='rosizolIcon'/></Link>
       </div>
       </div>
       <button type="button" onClick={HandleSldBtnClick} className="slide-link"><FaQuestion/></button>
